8152d869b5e4bc2ef85336166f969f62d0962c56
Evseev Oleg <oleg063053@gmail.com>
2021-11-06 20:34:51 +0400

    1. Servlet. Web.xml [#6866]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -6,16 +6,16 @@ import ru.job4j.dream.model.Post;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * Класс синглтон, хранит 3 объекта класса Post()
+ * Класс синглтон, при создании обьекта заполняет две
+ * мапы posts и candidates по 3 обьекта.
  */
 public class Store {
-
+    private static AtomicInteger POST_ID = new AtomicInteger(4);
     private static final Store INST = new Store();
-
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
-
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private Store() {
@@ -37,5 +37,10 @@ public class Store {
 
     public Collection<Candidate> findAllCandidates() {
         return candidates.values();
+    }
+
+    public void save(Post post) {
+        post.setId(POST_ID.incrementAndGet());
+        posts.put(post.getId(), post);
     }
 }
